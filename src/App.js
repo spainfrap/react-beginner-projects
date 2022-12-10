93e44609e4667a6f1e85d404f0a777f94aaea333
spainfrap <82938109+spainfrap@users.noreply.github.com>
2022-12-10 12:18:45 +0300

    task counter done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import './index.scss';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onCountPlus = () => {
+    setCount((count) => count + 1);
+  };
+
+  const onCountMinus = () => {
+    setCount((count) => count - 1);
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={onCountMinus} className="minus">
+          - Минус
+        </button>
+        <button onClick={onCountPlus} className="plus">
+          Плюс +
+        </button>
       </div>
     </div>
   );
