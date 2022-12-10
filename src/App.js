425bd6986d73f0233a1a1a62aae63245ae57da07
spainfrap <82938109+spainfrap@users.noreply.github.com>
2022-12-10 13:40:39 +0300

    task quiz initial

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './index.scss';
 
 const questions = [
@@ -8,7 +9,11 @@ const questions = [
   },
   {
     title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    variants: [
+      'приложение',
+      'часть приложения или страницы',
+      'то, что я не знаю что такое',
+    ],
     correct: 1,
   },
   {
@@ -32,26 +37,40 @@ function Result() {
   );
 }
 
-function Game() {
+function Game({ question, onClickVariant }) {
   return (
     <>
       <div className="progress">
         <div style={{ width: '50%' }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((text, index) => (
+          <li
+            onClick={() => {
+              onClickVariant(index);
+            }}
+            key={text}
+          >
+            {text}
+          </li>
+        ))}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step, setStep] = useState(0);
+  const question = questions[step];
+
+  const onClickVariant = (index) => {
+    console.log(step, index);
+  };
+
   return (
     <div className="App">
-      <Game />
+      <Game question={question} onClickVariant={onClickVariant} />
       {/* <Result /> */}
     </div>
   );
